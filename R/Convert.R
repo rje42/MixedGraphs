b31b9152a6328ca0e858b12e45007462bc8ef13b
Robin Evans <evans@stats.ox.ac.uk>
2016-02-09 16:56:00 +0000

    Additional functionality to convert()

--- R/Convert.R
+++ R/Convert.R
@@ -1,4 +1,5 @@
-## functions for using graph, igraph, gR, ...
+## functions for using graph, igraph, PAG, ggm, igraph, bn
+## Really need to tidy this up
 
 ##' Convert graph to format associated with specific package
 ##' 
@@ -8,20 +9,30 @@
 ##' from the object in many cases
 ##' @param ...
 ##' 
-##' @details Currently limited functionality, only
-##' converts 'ADMGs' and 'ggm' to 'mixedgraph' objects.
+##' @details Currently limited functionality.  Possible formats
+##' are "ADMG", "ggm", "graphNEL", "graphAM", "PAG", "bn", "igraph",
+##' and of course "mixedgraph".
+##' The function can recognise the class of objects other than
+##' "ggm", which is just an adjacency matrix.
 ##' 
 ##' Implemented thus far:
-##' mixedgraph <-> ggm
-##' mixedgraph <-> ADMG
-##' mixedgraph <-> graphNEL
-##' mixedgraph <-> graphAM
-##' mixedgraph <-- PAG
+##' \itemize{
+##' \item mixedgraph <-> ggm
+##' \item mixedgraph <-> ADMG
+##' \item mixedgraph <-> graphNEL
+##' \item mixedgraph <-> graphAM
+##' \item mixedgraph <-- PAG
+##' \item mixedgraph <-> bn
+##' \item mixedgraph <-- igraph
+##' }
 ##' 
 ##' \code{ggm} entries must be specified by \code{cur_format = "ggm"}.
 ##' 
 ##' @export convert
 convert <- function(graph, format="mixedgraph", cur_format, ...) {
+  
+  ## formats we can do 'to' and 'from' mixedgraph.
+  via_mixed_graph <- c("ggm", "ADMG", "graphNEL", "graphAM", "bn")
   
   if (missing(cur_format)) {
     if (class(graph) == "igraph") cur_format <- "igraph"
@@ -32,10 +43,13 @@ convert <- function(graph, format="mixedgraph", cur_format, ...) {
     else if (class(graph) == "graph") {
       cur_format <- "ADMG"
     }
+    else if (class(graph) == "bn") cur_format <- "bn"
     else {
       stop("Format not supported")
     }
   }
+  
+  if (format == cur_format) return(graph)
   
   if (cur_format == "ADMG") {
     if(format == "mixedgraph") {
@@ -45,8 +59,9 @@ convert <- function(graph, format="mixedgraph", cur_format, ...) {
       edges <- edges[!sapply(edges, is.null)]
       out = mixedgraph(n, v=seq_len(n), edges = edges, vnames=graph$vnames)
     } 
-    else if (cur_format %in% c("ggm")) {
-      Recall(Recall(graph, "mixedgraph", cur_format=cur_format), format=format, "mixedgraph")
+    else if (cur_format %in% via_mixed_graph) {
+      ## go 'via' a mixedgraph
+      out <- Recall(Recall(graph, "mixedgraph", cur_format=cur_format), format=format, "mixedgraph")
     }
     else {
       stop("Method not currently supported, but check back soon...")
@@ -82,8 +97,9 @@ convert <- function(graph, format="mixedgraph", cur_format, ...) {
       
       out <- mixedgraph(nv, edges = edges, vnames = colnames(graph))
     }    
-    else if (cur_format %in% c("ADMG")) {
-      Recall(Recall(graph, "mixedgraph", cur_format=cur_format), format=format, "mixedgraph")
+    else if (cur_format %in% via_mixed_graph) {
+      ## go 'via' a mixedgraph
+      out <- Recall(Recall(graph, "mixedgraph", cur_format=cur_format), format=format, "mixedgraph")
     }
     else {
       stop("Method not currently supported, but check back soon...")
@@ -99,8 +115,9 @@ convert <- function(graph, format="mixedgraph", cur_format, ...) {
       names(edgeList) <- edgemode(graph)
       out <- mixedgraph(n=length(vnames), vnames=vnames, edges=edgeList)
     }
-    else if (cur_format %in% c("ggm", "ADMG")) {
-      Recall(Recall(graph, "mixedgraph", cur_format=cur_format), format=format, "mixedgraph")
+    else if (cur_format %in% via_mixed_graph) {
+      ## go 'via' a mixedgraph
+      out <- Recall(Recall(graph, "mixedgraph", cur_format=cur_format), format=format, "mixedgraph")
     }
     else {
       stop("Method not currently supported, but check back soon...")
@@ -115,7 +132,7 @@ convert <- function(graph, format="mixedgraph", cur_format, ...) {
       out <- mixedgraph(n=length(vnames), vnames=vnames, edges=edgeMat)
     }
     else if (cur_format %in% c("ggm", "ADMG")) {
-      Recall(Recall(graph, "mixedgraph", cur_format=cur_format), format=format, "mixedgraph")
+      out <- Recall(Recall(graph, "mixedgraph", cur_format=cur_format), format=format, "mixedgraph")
     }
     else {
       stop("Method not currently supported, but check back soon...")
@@ -147,6 +164,49 @@ convert <- function(graph, format="mixedgraph", cur_format, ...) {
     
     out <- mixedgraph(n = ncol(graph), vnames = colnames(graph), edges=edges)
     
+    if (format != "mixedgraph") out <- Recall(out, format=format)
+  }
+  else if (cur_format == "igraph") {
+    require(igraph)
+
+    am_sp <- graph[]
+    if (nrow(am_sp) > 500) warning("Large graph, might be inefficient to use non-sparse adjacency matrix")
+    am <- as.matrix(am_sp)  # get adjacency matrix
+    ud <- am*(am == t(am))
+    dir <- am - ud
+    if (igraph::is_named(g)) {
+      vnames <- V(graph)$name
+    }
+    else vnames <- NULL
+    
+    out <- mixedgraph(n=length(V(graph)), vnames=vnames, edges=list(undirected=ud, directed=dir))
+    if (format != "mixedgraph") out <- Recall(out, format=format)
+  }
+  else if (cur_format == "bn") {
+    if (format == "graphNEL") {
+      require(bnlearn)
+      return(bnlearn::as.graphNEL(graph))
+    }
+    else if (format == "graphAM") {
+      require(bnlearn)
+      return(bnlearn::as.graphAM(graph))
+    }
+    
+    vnames <- names(graph$nodes)
+    un <- sapply(graph$nodes, FUN = function(x) match(setdiff(x$nb, c(x$children, x$parents)), vnames))
+    names(un) <- NULL
+    un <- mapply(function(x,y) x[x > y], un, seq_along(un))
+    un <- mapply(function(x,y) lapply(x, function(z) c(y,z)), un, seq_along(un))
+    un <- do.call(c, un)
+
+    dir <- sapply(graph$nodes, FUN = function(x) match(x$children, vnames))
+    names(dir) <- NULL
+    dir <- mapply(function(x,y) lapply(x, function(z) c(y,z)), dir, seq_along(dir))
+    dir <- do.call(c, dir)
+    
+    edgeList <- list(undirected=un, directed=dir)
+    
+    out <- mixedgraph(n = length(vnames), vnames=vnames, edges = edgeList)
     if (format != "mixedgraph") out <- Recall(out, format=format)
   }
   else if (cur_format == "mixedgraph") {
@@ -210,6 +270,23 @@ convert <- function(graph, format="mixedgraph", cur_format, ...) {
       colnames(amat) <- graph$vnames[graph$v]
       
       out <- graphAM(amat, edgemode = mode)
+    }
+    else if (format == "bn") {
+      require(bnlearn)
+      arcs1 <- edgeMatrix(graph$edges$directed)
+      ## undirected edges are just directed both ways
+      arcs2 <- edgeMatrix(graph$edges$undirected)
+      arcs2 <- cbind(arcs2, arcs2[2:1,])
+      ## make this into matrix of the form used 
+      ## by bnlearn
+      edges <- rbind(t(arcs1), t(arcs2))
+      edges[] <- graph$vnames[edges]
+      colnames(edges) <- c("from", "to")
+      
+      ## make a new bn object
+      out <- bnlearn::empty.graph(graph$vnames[graph$v])
+      out$arcs <- edges
+      out$nodes <- bnlearn:::cache.structure(graph$vnames[graph$v], edges)
     }
     else {
       stop("Method not currently supported, but check back soon...")
