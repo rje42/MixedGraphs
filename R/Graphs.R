555bd6b8c3bf5947be58563a48b901f7efd6316a
Robin Evans <evans@stats.ox.ac.uk>
2016-07-20 17:25:13 +0100

    Move .onLoad to end of R folder

--- R/Graphs.R
+++ R/Graphs.R
@@ -1,12 +1,3 @@
-.onLoad <- function(libname, pkgname) {
-  vig_list = tools::vignetteEngine(package = 'knitr')
-  vweave <- vig_list[['knitr::knitr']][c('weave')][[1]]
-  vtangle <- vig_list[['knitr::knitr']][c('tangle')][[1]]
-  tools::vignetteEngine(pkgname, weave = vweave, tangle = vtangle,
-                        pattern = "[.]Rmd$", package = pkgname)
-  #register_vignette_engines(pkgname)
-}
-
 graphOptionsEnv <- new.env()
 
 ## List of edge types, which can be expanded.
